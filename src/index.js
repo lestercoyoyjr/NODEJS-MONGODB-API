40ca23eac842a3d6218133b930565be9889c576f
lestercoyoy <lester_cch@hotmail.com>
2022-10-19 22:29:11 -0600

    ADD
    Route get to test server response

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ const express = require("express")
 const app = express();
 const port = process.env.PORT || 9000;
 
+// routes
+app.get('/', (req,res) => {
+    res.send('Welcome to my API');
+})
+
 app.listen(port, () => console.log('server listening on port ', port))
